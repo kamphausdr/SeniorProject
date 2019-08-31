ec242de26d075bfa9ea51020ec88d3e244347ca0
Derrick <kamphausdr@csustudent.net>
2019-08-31 11:23:01 -0400

    uploading project

---
+++ SpiderGameProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Defines how fast the player moves, can be adjusted in editor
+    public float MovementFactor = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Create a 2D Vector to track players position
+        Vector2 pos = transform.position;
+        float horizontal = Input.GetAxis("Horizontal"); // Provides the input for the player, designed to work with any device
+        // move the player by the movement factor, moves in the direction the player is inputing
+        pos.x = pos.x + MovementFactor * horizontal * Time.deltaTime; // Delta time ensures game is synced with framerate so cpu speed doesnt effect speed
+        transform.position = pos;
+    }
+}
